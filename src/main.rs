d0d192c5868a72c957be851d9934495c604e4fad
Łukasz Hryniuk <code@hryniuk.pl>
2018-03-17 22:38:01 +0100

    Fix file reading

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,39 @@
-mod memory;
+use std::io::{BufRead,BufReader};
+use std::fs::File;
+use std::env;
+use std::process;
+
+mod ec;
+mod mem;
+mod record;
+
+fn usage(name: &String) {
+    println!("Usage:\n\t{} <alf_path>", name);
+}
+
+fn check_line(line: String) {
+    println!("{}", line);
+}
 
 fn main() {
-    let memory = memory::new();
-    println!("x = {}", memory.mem[0]);
-    println!("x = {}", memory.get_gpr(1));
+    let args: Vec<String> = env::args().collect();
+    if args.len() <= 1 {
+        usage(&args[0]);
+        process::exit(0);
+    }
+
+    let alf_path = &args[1];
+
+    let reader = BufReader::new(File::open(alf_path).expect
+        ("Cannot open file"));
+
+    for line in reader.lines() {
+        match line {
+            Ok(v) => check_line(v),
+            Err(e) => println!("Error on reading {} file: {}", &alf_path, e),
+        }
+    }
+
+    let ecc = ec::Ec{mem : mem::new()};
+
 }
