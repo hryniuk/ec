69391d2b0fc34c5f346af0bc76e17869c32b9f88
Łukasz Hryniuk <code@hryniuk.pl>
2018-04-02 21:50:22 +0200

    Add basic Record checksum checking

--- src/alf/record.rs
+++ src/alf/record.rs
@@ -1,5 +1,5 @@
 //use std::vec::Vec;
-use std::io::Error;
+use std::io::{Error,ErrorKind};
 
 
 pub struct Record {
@@ -9,14 +9,28 @@ pub struct Record {
 
 impl Record {
     pub fn from(record_line: String) -> Result<Record, Error> {
+        // TODO: consider converting string to Vec<Ops>
+        if !Record::is_valid(&record_line) {
+            return Err(Error::new(ErrorKind::Other, "invalid record line"));
+        }
+
         Ok(Record{data: record_line})
     }
 
-    fn verify_checksum(record_line: String) -> bool {
-        //let checksum = record_line;
-        //let characters_sum: i32 = 0;
-        //println!("{}", (record_line.chars().map(|c| c.to_digit(16).unwrap()).sum::<u32>() - 14) % 16);
-        return true;
+    fn read_checksum(record_line: &String) -> Option<u32> {
+        record_line.chars().next().unwrap().to_digit(16)
+    }
+
+    fn calculate_checksum(record_line: &String) -> u32 {
+        (record_line[1..].chars().map(|c| c.to_digit(16).unwrap()).sum::<u32>()) % 16
+    }
+
+    fn is_valid(record_line: &String) -> bool {
+        // TODO: check if all characters are [0-9A-FN]
+        match Record::read_checksum(record_line) {
+            Some(checksum) => checksum == Record::calculate_checksum(record_line),
+            None => false,
+        }
     }
 }
 
@@ -26,8 +40,16 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_verify_checksum() {
-        let checksum = String::from("E10241A2301020304207FF1BEC");
-        assert!(Record::verify_checksum(checksum));
+    fn test_result_is_constructed_with_valid_record_line() {
+        let valid_record_line = String::from("E10241A2301020304207FF1BEC");
+        let record = Record::from(valid_record_line);
+        assert!(record.is_ok());
+    }
+
+    #[test]
+    fn test_error_is_reported_on_invalid_checksum() {
+        let invalid_checksum_line = String::from("E20241A2301020304207FF1BEC");
+        let record = Record::from(invalid_checksum_line);
+        assert!(record.is_err());
     }
 }
