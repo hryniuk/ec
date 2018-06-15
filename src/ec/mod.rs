eabacb319237b42b24a3e100bc65d72f538b04cb
Łukasz Hryniuk <code@hryniuk.pl>
2018-06-15 12:53:26 +0200

    Add exception type with values list

--- src/ec/mod.rs
+++ src/ec/mod.rs
@@ -2,6 +2,30 @@
 pub mod alf;
 pub mod mem;
 
+/// Type representing error arising during
+/// course of instruction execution
+pub enum EcError {
+    /// At the start of an instruction execution cycle
+    /// the ILC does not contain an even value.
+    IllegalInstructionAddress,
+    /// There is no operation defined for this operation code.
+    UnimplementedInstruction,
+    /// The indirect address is not even.
+    InvalidIndirectAddress,
+    /// The address of a purported word operand to an
+    /// instruction is not divisible by four.
+    WordAddressingError,
+    /// The result of some real-valued operation cannot be
+    /// expressed within the format for normalized real numbers.
+    UnrepresentableRealValue,
+    /// The effective address of an Execute instruction is not even.
+    InvalidExecutionAddress,
+    /// The divisor in a division or remainder operation is zero.
+    ZeroDivisor,
+    /// A four-character instruction begins at FFFE.
+    WraparoundInstruction,
+}
+
 pub struct Ec {
     pub mem: mem::Memory,
 }
