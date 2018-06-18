7a64316e672e22cbec98cdd2bb7240831e0beaa6
Łukasz Hryniuk <code@hryniuk.pl>
2018-06-18 22:49:24 +0200

    Add mem member to Cpu

--- src/ec/cpu/mod.rs
+++ src/ec/cpu/mod.rs
@@ -1,10 +1,17 @@
+mod instruction;
+
+use ec::mem;
+use std::cell::RefCell;
+use std::rc::Rc;
+
 pub struct Cpu {
     ilc: u32,
+    mem: Rc<RefCell<mem::Memory>>,
 }
 
 impl Cpu {
-    pub fn new() -> Cpu {
-        Cpu { ilc: 0 }
+    pub fn new(mem: Rc<RefCell<mem::Memory>>) -> Cpu {
+        Cpu { ilc: 0, mem }
     }
     pub fn poll() {}
 }
