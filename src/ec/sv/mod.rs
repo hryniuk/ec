03ee733d368221f767ac6b02322d4d3cfabc0650
Łukasz Hryniuk <code@hryniuk.pl>
2018-08-31 10:46:03 +0200

    Add ReadInt supervisor call

--- src/ec/sv/mod.rs
+++ src/ec/sv/mod.rs
@@ -7,6 +7,9 @@ pub enum Action {
     None,
     /// Exit the running program and clean up after it.
     Exit,
+    /// Read the integer from the stdin and store it at the effective address
+    /// of the SVC
+    ReadInt(cpu::instruction::Address),
     /// Write the word at the effective address as an integer
     /// on the output stream.
     WriteInt(cpu::instruction::Address),
@@ -25,9 +28,13 @@ impl Supervisor {
             // TODO: consider "output buffer" to make testing easier
             match cpu.poll(true) {
                 Ok(Action::Exit) => break,
+                Ok(Action::ReadInt(addr)) => {
+                    let read_value: i32 = read!();
+                    self.mem.borrow_mut().write_word(addr as usize, read_value);
+                },
                 Ok(Action::WriteInt(addr)) => {
                     println!("{}", self.mem.borrow().read_word(addr as usize));
-                }
+                },
                 Ok(Action::None) => (),
                 Err(_) => break,
             }
